87087beca0fbaa69425ac41b4420216467ad5d91
Caroline Cagnin <cagnin.caroline@gmail.com>
2024-03-14 13:23:27 -0300

    create dev branch

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
-const mysql = require("mysql");
+const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
 });
 
 async function connectToDatabase() {
