b3147d2d94105df7addac9c5d3dcf647b17894a0
Caroline Cagnin <cagnin.caroline@gmail.com>
2024-03-18 13:30:03 -0300

    add mysql variables

--- src/config/database.js
+++ src/config/database.js
@@ -1,11 +1,11 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_DATABASE,
+  host: process.env.MYSQLHOST,
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
 });
 
 async function connectToDatabase() {
