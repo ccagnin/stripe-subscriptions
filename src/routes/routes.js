a1d3f7de3e01c7f52b55750362e40f6cb41e2e81
Caroline Cagnin <cagnin.caroline@gmail.com>
2024-04-02 18:36:18 -0300

    create cancel subscription endpoint

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const { paymentController } = require("../controllers/paymentController");
+const {
+  cancelSubscriptionController,
+} = require("../controllers/cancelSubscriptionController");
 
 router.post("/pay", paymentController);
+router.patch("/cancel", cancelSubscriptionController);
 
 module.exports = router;
