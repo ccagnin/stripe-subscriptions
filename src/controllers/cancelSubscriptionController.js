5668514bcdd28206886eb4869435b7f517bfa0b1
Caroline Cagnin <cagnin.caroline@gmail.com>
2024-05-24 17:14:46 -0300

    fix: cancel flow

--- src/controllers/cancelSubscriptionController.js
+++ src/controllers/cancelSubscriptionController.js
@@ -7,12 +7,16 @@ const { getSubscriptionBySubId } = require("../services/subscriptionService");
 const cancelSubscriptionController = async (req, res) => {
   const { subscriptionId } = req.body;
   try {
+    console.log("Subscription ID:", subscriptionId);
     const subscription = await getSubscriptionBySubId(subscriptionId);
+    console.log("Subscription found:", subscription);
 
+    console.log("Canceling subscription...");
     const cancelledSubscription = await stripe.subscriptions.cancel(
-      subscription.stripeSubscriptionId,
+      subscription.id,
     );
 
+    console.log("Subscription canceled:", cancelledSubscription);
     await prisma.subscription.update({
       where: {
         id: subscriptionId,
@@ -21,7 +25,7 @@ const cancelSubscriptionController = async (req, res) => {
         status: "canceled",
       },
     });
-
+    console.log("Subscription status updated in database");
     res.json({ subscription: cancelledSubscription });
   } catch (error) {
     res.status(500).json({ error: error.message });
